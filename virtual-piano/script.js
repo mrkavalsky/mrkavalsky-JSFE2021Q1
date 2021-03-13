f76e471f3a09a6c15136e26086e1997f3654b680
mrkavalsky <mr.kavalsky@gmail.com>
2021-03-13 11:52:13 +0300

    feat: add animation on button click

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -1,7 +1,19 @@
-document.addEventListener('click', function (e) {
+const keys = document.querySelectorAll('.piano-key');
+
+function removeTransition(e) {
+    if(e.propertyName == 'transform')
+    this.classList.remove('active');
+}
+
+function playSound(e) {
     const note = e.target.dataset.note;
     const audio = document.querySelector(`audio[data-note="${note}"]`);
+    const key = document.querySelector(`.piano-key[data-note="${note}"]`);
     if(!audio) return;
     audio.currentTime = 0;
     audio.play();
-});
+    key.classList.add('active');
+}
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+document.addEventListener('click', playSound);
