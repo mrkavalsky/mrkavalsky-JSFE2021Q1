31937bf7e1872dd9eb3ee1408aa03b7a2b0981b1
mrkavalsky <mr.kavalsky@gmail.com>
2021-04-09 14:46:55 +0300

    feat: implement Reset button

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -1,11 +1,26 @@
 const filters = document.querySelector('.filters');
+const resetBtn = document.getElementById('btn-reset');
 
-function handleFilters(e) {
-  const input = e.target;
+function handleFilters(input) {
   const suffix = input.dataset.sizing;
-  const output = e.target.nextSibling.nextSibling;
+  const output = input.nextSibling.nextSibling;
   output.value = input.value;
   document.documentElement.style.setProperty(`--${input.name}`, input.value + suffix);
 }
 
-filters.addEventListener('input', handleFilters);
+function resetFilters() {
+  const arr = [...filters.children];
+  arr.forEach(label => {
+    const input = label.children[0];
+    input.value = input.defaultValue;
+    handleFilters(input);
+  });
+}
+
+function getInput(e) {
+  const input = e.target;
+  handleFilters(input);
+}
+
+filters.addEventListener('input', getInput);
+resetBtn.addEventListener('click', resetFilters);
