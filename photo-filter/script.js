9b5f979b9699746a7591a3dfe54faa1c339091f1
mrkavalsky <mr.kavalsky@gmail.com>
2021-04-10 14:41:02 +0300

    feat: implement download images function

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -3,10 +3,13 @@ const resetBtn = document.getElementById('btn-reset');
 const nextBtn = document.getElementById('btn-next');
 const loadBtn = document.getElementById('btnInput');
 const image = document.getElementById('image');
+const saveBtn = document.getElementById('btn-save');
 const fullScrnBrn = document.getElementById('fullscreen');
 const base = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/';
 const images = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
 let i = 0;
+
+image.setAttribute('crossOrigin', 'anonymous'); 
 
 function handleFilters(input) {
   const suffix = input.dataset.sizing;
@@ -82,9 +85,28 @@ function setFullScreen() {
   }
 }
 
+function drawImage() {
+  const canvas = document.createElement('canvas');
+  canvas.width = image.naturalWidth;
+  canvas.height = image.naturalHeight;
+  const context = canvas.getContext('2d');
+  context.drawImage(image, 0, 0);
+  const dataURL = canvas.toDataURL("image/png");
+  downloadFile(dataURL);
+}
+
+function downloadFile(dataURL) {
+  const link = document.createElement('a');
+  link.download = 'download.png';
+  link.href = dataURL;
+  link.click();
+  link.delete;
+}
+
 filters.addEventListener('input', getInput);
 resetBtn.addEventListener('click', resetFilters);
 nextBtn.addEventListener('click', getImage);
 nextBtn.addEventListener('click', resetInput);
 loadBtn.addEventListener('change', getFile);
 fullScrnBrn.addEventListener('click', setFullScreen);
+saveBtn.addEventListener('click', drawImage);
