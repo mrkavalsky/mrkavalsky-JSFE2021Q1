a79fc87bc8aa3a5933d78582ca4a1c16355975e2
mrkavalsky <mr.kavalsky@gmail.com>
2021-04-10 11:38:52 +0300

    feat: add fullscreen mode

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -3,6 +3,7 @@ const resetBtn = document.getElementById('btn-reset');
 const nextBtn = document.getElementById('btn-next');
 const loadBtn = document.getElementById('btnInput');
 const image = document.getElementById('image');
+const fullScrnBrn = document.getElementById('fullscreen');
 const base = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/';
 const images = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
 let i = 0;
@@ -73,8 +74,17 @@ function resetInput() {
   loadBtn.value = '';
 }
 
+function setFullScreen() {
+  if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen();
+  } else {
+      document.exitFullscreen();
+  }
+}
+
 filters.addEventListener('input', getInput);
 resetBtn.addEventListener('click', resetFilters);
 nextBtn.addEventListener('click', getImage);
 nextBtn.addEventListener('click', resetInput);
 loadBtn.addEventListener('change', getFile);
+fullScrnBrn.addEventListener('click', setFullScreen);
