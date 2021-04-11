c80da9fb02d9240f777c703b09ff685ca02f6544
mrkavalsky <mr.kavalsky@gmail.com>
2021-04-11 14:51:07 +0300

    refactor: add attribute crossOrigin to image

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -9,8 +9,6 @@ const base = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tas
 const images = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
 let i = 0;
 
-image.setAttribute('crossOrigin', 'anonymous'); 
-
 function handleFilters(input) {
   const suffix = input.dataset.sizing;
   const output = input.nextSibling.nextSibling;
