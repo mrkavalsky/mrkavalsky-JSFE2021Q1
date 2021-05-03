aa41a70ff4a2458b309137c16a0b4f51d8de7375
mrkavalsky <mr.kavalsky@gmail.com>
2021-05-03 12:41:48 +0300

    feat: implement replaceVideo function

--- online-zoo/pages/zoos-translation/panda/panda.js
+++ online-zoo/pages/zoos-translation/panda/panda.js
@@ -1,4 +1,6 @@
 const accordion = document.querySelector('.accordion');
+const frame = document.getElementById('main-frame');
+const slider = document.getElementById('slider');
 
 function toggleAccordion(e) {
   if(!e.target.matches('.accordion__button')) return;
@@ -6,4 +8,13 @@ function toggleAccordion(e) {
   e.target.closest('li').lastElementChild.classList.toggle('accordion-wrapper__p_hidden');
 }
 
+function replaceVideo(e) {
+  const item = e.target.closest('.slider__item');
+  if(!item) return;
+  const src = item.lastElementChild.src + '?autoplay=1';
+  item.lastElementChild.src = frame.src.slice(0, frame.src.length - 11);
+  frame.src = src;
+}
+
 accordion.addEventListener('click', toggleAccordion);
+slider.addEventListener('click', replaceVideo);
