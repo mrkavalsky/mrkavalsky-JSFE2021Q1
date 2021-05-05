7f29b986f446057d9b918ed0b5c9da84cbca9577
mrkavalsky <mr.kavalsky@gmail.com>
2021-05-05 20:15:12 +0300

    feat: add gray color to the accordion borders

--- online-zoo/pages/zoos-translation/panda/panda.js
+++ online-zoo/pages/zoos-translation/panda/panda.js
@@ -6,6 +6,7 @@ function toggleAccordion(e) {
   if(!e.target.matches('.accordion__button')) return;
   e.target.classList.toggle('accordion__button_hidden');
   e.target.closest('li').lastElementChild.classList.toggle('accordion-wrapper__p_hidden');
+  e.target.closest('li').classList.toggle('accordion__item_gray-border');
 }
 
 function replaceVideo(e) {
