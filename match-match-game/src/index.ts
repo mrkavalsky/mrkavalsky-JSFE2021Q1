02c89b9f57a2a222e0e1ed633e642155bdb31cfe
mrkavalsky <mr.kavalsky@gmail.com>
2021-05-14 19:49:19 +0300

    feat: implement the BaseBlock class

--- match-match-game/src/index.ts
+++ match-match-game/src/index.ts
@@ -1,8 +1,8 @@
 import { App } from './app';
-import { Header } from './components/header/header';
+import { BaseBlock } from './components/base-block';
 import './index.css';
 
 window.addEventListener('DOMContentLoaded', () => {
-  const matchMatchGameApp: App = new App(document.body);
-  matchMatchGameApp.appendComponent(new Header());
+  const matchMatchApp: App = new App(document.body);
+  matchMatchApp.appendComponent(new BaseBlock('header', ['header']));
 })
