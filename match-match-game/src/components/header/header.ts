b91a04d209e95e7b3a23589a094f4f20a0af74c8
mrkavalsky <mr.kavalsky@gmail.com>
2021-05-14 19:48:48 +0300

    fix: remove the header class

--- match-match-game/src/components/header/header.ts
+++
@@ -1,8 +0,0 @@
-import { BaseComponent } from "../base-component";
-import './header.css';
-
-export class Header extends BaseComponent {
-  constructor() {
-    super('header', ['header']);
-  }
-}
