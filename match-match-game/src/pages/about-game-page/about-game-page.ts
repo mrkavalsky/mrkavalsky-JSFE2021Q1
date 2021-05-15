d65e324a83b240feb659b6404da39453f01922ba
mrkavalsky <mr.kavalsky@gmail.com>
2021-05-15 15:41:57 +0300

    feat: add the basic CSS styles to the about-game-page.css file

--- match-match-game/src/pages/about-game-page/about-game-page.ts
+++ match-match-game/src/pages/about-game-page/about-game-page.ts
@@ -10,15 +10,15 @@ export class AboutGamePage extends BaseBlock {
     this.appendComponents([new BaseBlock('div', ['main__content-wrapper'])]);
     this.children[0].element.innerHTML = `
       <ul class="rules-list">
-        <li class="rules-list__item1">
+        <li class="rules-list__item rules-list__item_item1">
           <div class="rules-list__count">1</div>
           <p class="rules-list__text">Register new player in game</p>
         </li>
-        <li class="rules-list__item2">
+        <li class="rules-list__item rules-list__item_item2">
           <div class="rules-list__count">2</div>
           <p class="rules-list__text">Configure your game settings</p>
         </li>
-        <li class="rules-list__item3"></li>
+        <li class="rules-list__item rules-list__item_item3"></li>
           <div class="rules-list__count">3</div>
           <p class="rules-list__text">Start you new game! Remember card positions and match it before times up</p>
       </ul>
