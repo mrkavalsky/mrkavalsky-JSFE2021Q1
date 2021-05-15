acea4899110a1b40f4b5e73e1a938e361023f789
mrkavalsky <mr.kavalsky@gmail.com>
2021-05-15 20:24:15 +0300

    feat: implement the BasePage class

---
+++ match-match-game/src/components/base-page.ts
@@ -0,0 +1,13 @@
+import { BaseBlock } from "./base-block";
+import { BaseComponent } from "./base-component";
+
+export class BasePage extends BaseBlock {
+  constructor(
+    tag: keyof HTMLElementTagNameMap = 'div',
+    styles: string[] = [],
+    children: BaseComponent[] = [],
+    public name: string
+  ) {
+    super(tag, styles, children);
+  }
+}
