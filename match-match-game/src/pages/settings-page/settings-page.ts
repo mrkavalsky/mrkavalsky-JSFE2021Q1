2d4390ed7efc01d6e27299f2eb4ccb4131c052f1
mrkavalsky <mr.kavalsky@gmail.com>
2021-05-15 18:09:29 +0300

    implement the basic layout of the Game Settings page

---
+++ match-match-game/src/pages/settings-page/settings-page.ts
@@ -0,0 +1,8 @@
+import { BaseBlock } from "../../components/base-block";
+import './settings-page.css';
+
+export class SettingsPage extends BaseBlock {
+  constructor() {
+    super('main', ['main'], []);
+  }
+}
