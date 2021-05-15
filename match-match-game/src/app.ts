eef7214da4c0adbe540395a22a3669f31b05a606
mrkavalsky <mr.kavalsky@gmail.com>
2021-05-15 15:43:59 +0300

    fix: fix the appendComponent method

--- match-match-game/src/app.ts
+++ match-match-game/src/app.ts
@@ -3,7 +3,7 @@ import { BaseComponent } from "./components/base-component";
 export class App {
   constructor(private readonly rootElement: HTMLElement, public rootChildren: BaseComponent[] = []) {}
   appendComponent(block: BaseComponent): void {
-    this.rootElement.append(block.component);
+    this.rootElement.append(block.element);
     this.rootChildren.push(block);
   }
 }
