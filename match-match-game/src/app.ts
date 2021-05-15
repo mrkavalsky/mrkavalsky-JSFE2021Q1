af6148ec64a277ed07fe89dcd3b3ccb4946b8dee
mrkavalsky <mr.kavalsky@gmail.com>
2021-05-15 16:11:23 +0300

    refactor: move the appendComponents method calls to the App class constructor

--- match-match-game/src/app.ts
+++ match-match-game/src/app.ts
@@ -1,7 +1,13 @@
+import { BaseBlock } from "./components/base-block";
 import { BaseComponent } from "./components/base-component";
+import { AboutGamePage } from "./pages/about-game-page/about-game-page";
+import './index.css';
 
 export class App {
-  constructor(private readonly rootElement: HTMLElement, public rootChildren: BaseComponent[] = []) {}
+  constructor(private readonly rootElement: HTMLElement, public rootChildren: BaseComponent[] = []) {
+    this.appendComponent(new BaseBlock('header', ['header']));
+    this.appendComponent(new AboutGamePage());
+  }
   appendComponent(block: BaseComponent): void {
     this.rootElement.append(block.element);
     this.rootChildren.push(block);
--- match-match-game/src/index.ts
+++ match-match-game/src/index.ts
@@ -1,10 +1,5 @@
 import { App } from './app';
-import { BaseBlock } from './components/base-block';
-import './index.css';
-import { AboutGamePage } from './pages/about-game-page/about-game-page';
 
 window.addEventListener('DOMContentLoaded', () => {
   const matchMatchApp: App = new App(document.body);
-  matchMatchApp.appendComponent(new BaseBlock('header', ['header']));
-  matchMatchApp.appendComponent(new AboutGamePage());
 })
