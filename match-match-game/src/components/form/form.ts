92e478b2f473ed7ae1e46d448f3f69c1e78c2b08
mrkavalsky <mr.kavalsky@gmail.com>
2021-05-16 20:23:33 +0300

    feat: add the CSS styles to the form

--- match-match-game/src/components/form/form.ts
+++ match-match-game/src/components/form/form.ts
@@ -2,12 +2,13 @@ import { BaseBlock } from "../base-block";
 import { BaseButton } from "../base-button";
 import { BaseComponent } from "../base-component";
 import { Input } from "../input/input";
+import './form.css';
 
 
 export class Form extends BaseBlock {
   public inputsArray: Input[] = [new Input('First Name', 'text'), new Input('Last Name', 'text'), new Input('E-mail', 'email')];
-  public submitButton: BaseButton = new BaseButton('button', ['form__button', 'form__button_submit']);
-  public cancelButton: BaseButton = new BaseButton('button', ['form__button', 'form__button_cancel']);
+  public submitButton: BaseButton = new BaseButton('button', ['form__button', 'form__button_submit'], 'add user');
+  public cancelButton: BaseButton = new BaseButton('button', ['form__button', 'form__button_cancel'] , 'cancel');
   constructor(
     tag: keyof HTMLElementTagNameMap = 'form',
     styles: string[] = ['form'],
