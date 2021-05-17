2034ad7fd94306f3b56d7a1685b8031b4cbe97cb
mrkavalsky <mr.kavalsky@gmail.com>
2021-05-17 14:19:56 +0300

    feat: implement the textInputValidation method in the input class

--- match-match-game/src/components/input/input.ts
+++ match-match-game/src/components/input/input.ts
@@ -6,8 +6,10 @@ export class Input extends BaseBlock {
   public caption: BaseComponent = new BaseComponent('div', ['input__caption']);
   public input: BaseComponent = new BaseComponent('input', ['input__field']);
   public error: BaseComponent = new BaseComponent('div', ['input__error']);
+  public inputType: string;
   constructor(caption: string, inputType: string) {
     super('div', ['input']);
+    this.inputType = inputType;
     this.caption.element.innerText = caption;
     this.input.element.setAttribute('type', inputType);
     this.appendComponents([this.caption, this.input, this.error]);
@@ -15,11 +17,17 @@ export class Input extends BaseBlock {
   }
   addValidationEvent(): void {
     this.input.element.addEventListener('input', () => {
-      const input = this.input.element as HTMLInputElement;
-      this.error.element.innerText = this.inputValidation(input.value);
+      this.error.element.innerText = this.inputType === 'text' ? this.textInputValidation() : this.textInputValidation();
     });
   }
-  inputValidation(inputValue: string): string {
-    return 'blablabla';
+  textInputValidation(): string {
+    const input:HTMLInputElement = this.input.element as HTMLInputElement;
+    const inputValue: string = input.value.trim();
+    const captionText = this.caption.element.innerText.toLowerCase();
+    if (inputValue.length === 0) return `!The ${captionText} cannot be empty`;
+    if (inputValue.length === inputValue.match(/\d/g)?.length) return `!The ${captionText} cannot consist only of numbers`;
+    if (/\s/.test(inputValue)) return `!The ${captionText} cannot contain more than one word`;
+    if (/[~!@#$%*()_—+=|:;"'`<>,.?/^]/.test(inputValue)) return `!The ${captionText} cannot contain service symbols`;
+    return 'Ok';
   }
 }
