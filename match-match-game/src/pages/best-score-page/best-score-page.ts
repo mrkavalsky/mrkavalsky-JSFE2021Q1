475206af7a7c50783372014c9fbbc995d858a2e2
mrkavalsky <mr.kavalsky@gmail.com>
2021-05-19 19:20:35 +0300

    feat: implement the addBestScoreTable method in the BestScorePage class

--- match-match-game/src/pages/best-score-page/best-score-page.ts
+++ match-match-game/src/pages/best-score-page/best-score-page.ts
@@ -1,4 +1,6 @@
 import { BasePage } from '../../components/base-page';
+import { IUser } from '../../components/user-interface';
+import { User } from '../../components/user/user';
 import './best-score-page.css';
 
 export class BestScorePage extends BasePage {
@@ -7,5 +9,11 @@ export class BestScorePage extends BasePage {
     this.element.innerHTML = `
       <h2 class="main__title">Best players</h2>
     `;
+  }
+
+  addBestScoreTable(users: IUser[]): void {
+    users.forEach((user) => {
+      this.appendComponents([new User(user)]);
+    })
   }
 }
