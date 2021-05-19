8c2211a71b1a78c4c2e482c0663a9bac315ddb00
mrkavalsky <mr.kavalsky@gmail.com>
2021-05-19 19:12:20 +0300

    feat: implement the user class

---
+++ match-match-game/src/components/user/user.ts
@@ -0,0 +1,23 @@
+import { BaseBlock } from "../base-block";
+import { BaseComponent } from "../base-component";
+import { IUser } from "../user-interface";
+
+export class User extends BaseBlock {
+  private userInfo: BaseBlock = new BaseBlock('div', ['user__info']);
+  private userScore: BaseBlock = new BaseBlock('div', ['user__score']);
+  constructor(private user: IUser) {
+    super('div', ['user']);
+    this.userInfo.appendComponents([
+      new BaseComponent('div', ['user__name'], `${this.user.firstName} ${this.user.lastName}`),
+      new BaseComponent('div', ['user__email'], `${this.user.email}`),
+    ]);
+    this.userScore.appendComponents([
+      new BaseComponent('div', ['user__title'], `Score:`),
+      new BaseComponent('div', ['user__result'], `${this.user.score}`),
+    ]);
+    this.appendComponents([
+      this.userInfo,
+      this.userScore,
+    ]);
+  }
+}
