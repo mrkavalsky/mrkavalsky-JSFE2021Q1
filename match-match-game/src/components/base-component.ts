ad342849c5e2e75ad352de8568b90ea03655e5c6
mrkavalsky <mr.kavalsky@gmail.com>
2021-05-19 16:29:41 +0300

    fix: change type of the tag var to string

--- match-match-game/src/components/base-component.ts
+++ match-match-game/src/components/base-component.ts
@@ -1,7 +1,7 @@
 export class BaseComponent {
   readonly element: HTMLElement;
 
-  constructor(tag: keyof HTMLElementTagNameMap = 'div', styles: string[] = []) {
+  constructor(tag: string = 'div', styles: string[] = []) {
     this.element = document.createElement(tag);
     this.element.classList.add(...styles);
   }
