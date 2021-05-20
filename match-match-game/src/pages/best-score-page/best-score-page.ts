5858c78bac40ecbc9421461d0506de658e554a34
mrkavalsky <mr.kavalsky@gmail.com>
2021-05-20 03:23:02 +0300

    feat: add the score block on the best score page

--- match-match-game/src/pages/best-score-page/best-score-page.ts
+++ match-match-game/src/pages/best-score-page/best-score-page.ts
@@ -1,11 +1,14 @@
 import { BasePage } from '../../components/base-page';
+import { ScoreBlock } from '../../components/score-block/score-block';
 import './best-score-page.css';
 
 export class BestScorePage extends BasePage {
+  public scoreBlock = new ScoreBlock();
   constructor() {
     super('main', ['main'], [], 'best-score');
     this.element.innerHTML = `
       <h2 class="main__title">Best players</h2>
     `;
+    this.appendComponents([this.scoreBlock]);
   }
 }
