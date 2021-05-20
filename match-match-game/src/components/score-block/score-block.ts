2801f6da618c9658c0f2ca0d3d96be02b0fa68ba
mrkavalsky <mr.kavalsky@gmail.com>
2021-05-20 03:20:28 +0300

    feat: implement the ScoreBlock class

---
+++ match-match-game/src/components/score-block/score-block.ts
@@ -0,0 +1,23 @@
+import { BaseBlock } from "../base-block";
+import { IUser } from "../user-interface";
+import { User } from "../user/user";
+
+export class ScoreBlock extends BaseBlock {
+  private userList: BaseBlock = new BaseBlock('div', ['score-block__users']);
+  constructor() {
+    super('div', ['score-block']);
+  }
+
+  getBestScoreList(users: IUser[] | void): void {
+    if (!users) return;
+    users.forEach((user) => {
+      this.userList.appendComponents([new User(user)]);
+    })
+  }
+  refreshBestScore(users: IUser[] | void): void {
+    this.element.lastElementChild?.remove();
+    this.userList = new BaseBlock('div', ['score-block__users']);
+    this.getBestScoreList(users);
+    this.element.append(this.userList.element);
+  }
+}
