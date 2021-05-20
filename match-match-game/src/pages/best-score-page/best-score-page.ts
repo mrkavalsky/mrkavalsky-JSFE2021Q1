1376d069f09bef2b11ce9049d54d057398118549
mrkavalsky <mr.kavalsky@gmail.com>
2021-05-20 03:10:01 +0300

    fix: remove the addBestScoreTable method from the BestScorePage class

--- match-match-game/src/pages/best-score-page/best-score-page.ts
+++ match-match-game/src/pages/best-score-page/best-score-page.ts
@@ -1,6 +1,4 @@
 import { BasePage } from '../../components/base-page';
-import { IUser } from '../../components/user-interface';
-import { User } from '../../components/user/user';
 import './best-score-page.css';
 
 export class BestScorePage extends BasePage {
@@ -9,12 +7,5 @@ export class BestScorePage extends BasePage {
     this.element.innerHTML = `
       <h2 class="main__title">Best players</h2>
     `;
-  }
-
-  addBestScoreTable(users: IUser[] | void): void {
-    if (!users) return;
-    users.forEach((user) => {
-      this.appendComponents([new User(user)]);
-    })
   }
 }
