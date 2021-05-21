40021360e9bf51ac7d1d917a16d9a69c3eacd0e6
mrkavalsky <mr.kavalsky@gmail.com>
2021-05-21 16:09:59 +0300

    feat: implement the constructor of the Card class

---
+++ match-match-game/src/components/card/card.ts
@@ -0,0 +1,19 @@
+import { BaseBlock } from '../base-block';
+import { BaseComponent } from '../base-component';
+import './card.css';
+
+export class Card extends BaseBlock {
+  public card: BaseBlock = new BaseBlock(
+    'div',
+    ['card'],
+    [
+      new BaseComponent('div', ['card__side', 'card__side_back']),
+      new BaseComponent('div', ['card__side', 'card__side_front']),
+    ],
+  );
+
+  constructor(path: string) {
+    super('div', ['card-container']);
+    this.appendComponents([this.card]);
+  }
+}
