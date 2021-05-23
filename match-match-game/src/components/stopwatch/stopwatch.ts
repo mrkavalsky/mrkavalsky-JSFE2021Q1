4e0eed78d9f9249144d6cff74bb0b87d9fa7d6e0
mrkavalsky <mr.kavalsky@gmail.com>
2021-05-23 21:28:10 +0300

    feat: implement the Stopwatch class

---
+++ match-match-game/src/components/stopwatch/stopwatch.ts
@@ -0,0 +1,32 @@
+import { BaseBlock } from '../../shared/base-block';
+import './stopwatch.css';
+
+export class Stopwatch extends BaseBlock {
+  private interval: NodeJS.Timeout | null = null;
+
+  private time = 0;
+
+  constructor() {
+    super('div', ['stopwatch']);
+    this.element.innerText = '0:00';
+  }
+
+  start(): void {
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+
+  tick(): void {
+    this.time++;
+    const minute = `${Math.floor(this.time / 60)}`;
+    const second = `${this.time % 60}`;
+    this.element.innerText = `${minute}:${
+      second.length === 1 ? `0${second}` : second
+    }`;
+  }
+
+  stop(): void {
+    if (!this.interval) return;
+    clearInterval(this.interval);
+    this.interval = null;
+  }
+}
