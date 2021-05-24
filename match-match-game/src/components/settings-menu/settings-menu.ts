c032e9bba6bcd74084816aee4c3a7f6f93a89777
mrkavalsky <mr.kavalsky@gmail.com>
2021-05-24 16:58:02 +0300

    feat: implement the SettingsMenu class

---
+++ match-match-game/src/components/settings-menu/settings-menu.ts
@@ -0,0 +1,31 @@
+import { BaseBlock } from '../../shared/base-block';
+import { BaseComponent } from '../../shared/base-component';
+import './settings-menu.css';
+
+export class SettingsMenu extends BaseBlock {
+  private caption: BaseComponent = new BaseComponent('div', [
+    'settings-menu__caption',
+  ]);
+
+  private select: BaseBlock = new BaseBlock('select', [
+    'settings-menu__select',
+  ]);
+
+  private optionsTitle: BaseComponent = new BaseComponent('option');
+
+  constructor(title: string, optionTitle: string, options: string[]) {
+    super('div', ['settings-menu']);
+    this.caption.element.innerText = title;
+    this.optionsTitle.element.innerText = optionTitle;
+    this.optionsTitle.element.setAttribute('disabled', 'disabled');
+    this.select.appendComponents([this.optionsTitle]);
+    this.select.appendComponents(
+      options.map((option) => new BaseComponent('option', [], option)),
+    );
+    this.appendComponents([this.caption, this.select]);
+  }
+
+  getMenuNode(): HTMLElement {
+    return this.select.element;
+  }
+}
