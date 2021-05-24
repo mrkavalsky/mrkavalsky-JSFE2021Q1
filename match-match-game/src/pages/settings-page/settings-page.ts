9bf75a613b647620f177a69b1f67771d100b3743
mrkavalsky <mr.kavalsky@gmail.com>
2021-05-24 17:43:29 +0300

    feat: add settings menu on the settings page

--- match-match-game/src/pages/settings-page/settings-page.ts
+++ match-match-game/src/pages/settings-page/settings-page.ts
@@ -1,8 +1,22 @@
+import { SettingsMenu } from '../../components/settings-menu/settings-menu';
 import { BasePage } from '../../shared/base-page';
 import './settings-page.css';
 
 export class SettingsPage extends BasePage {
+  private cardsSettings = new SettingsMenu(
+    'Game cards',
+    'select game cards type',
+    ['cats', 'animals'],
+  );
+
+  private difficultySettings = new SettingsMenu(
+    'Difficulty',
+    'select game type',
+    ['4', '6', '8'],
+  );
+
   constructor() {
     super('main', ['settings'], [], 'game-settings');
+    this.appendComponents([this.cardsSettings, this.difficultySettings]);
   }
 }
