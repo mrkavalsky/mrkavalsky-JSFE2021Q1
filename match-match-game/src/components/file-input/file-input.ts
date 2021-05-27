1710d5a6e0c7e72465c2fbfc04f9597df0227c13
mrkavalsky <mr.kavalsky@gmail.com>
2021-05-27 18:25:44 +0300

    feat: implement upload file to the imageSrc variable in the InputFile class

--- match-match-game/src/components/file-input/file-input.ts
+++ match-match-game/src/components/file-input/file-input.ts
@@ -7,9 +7,27 @@ export class FileInput extends BaseBlock {
     'file-input__input',
   ]);
 
+  private imageSrc: string | ArrayBuffer | null = null;
+
   constructor() {
     super('label', ['file-input']);
     this.input.element.setAttribute('type', 'file');
+    this.input.element.setAttribute('accept', 'image/*');
+    this.input.element.addEventListener('change', () => {
+      this.uploadFile();
+    });
     this.appendComponents([this.input]);
+  }
+
+  uploadFile(): void {
+    const inputElement: HTMLInputElement = this.input
+      .element as HTMLInputElement;
+    const fileList: FileList | null = inputElement.files;
+    if (!fileList) return;
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imageSrc = reader.result;
+    };
+    reader.readAsDataURL(fileList[0]);
   }
 }
