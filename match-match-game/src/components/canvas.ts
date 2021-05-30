f138b55e318508b1d2f82390b21329f37a0d66ac
mrkavalsky <mr.kavalsky@gmail.com>
2021-05-30 22:54:24 +0300

    fix: draw a user avatar in the center of the canvas element

--- match-match-game/src/components/canvas.ts
+++ match-match-game/src/components/canvas.ts
@@ -4,23 +4,17 @@ export class Canvas {
   private context: CanvasRenderingContext2D | null = null;
 
   constructor() {
-    this.canvas.width = 40;
-    this.canvas.height = 40;
     this.context = this.canvas.getContext('2d');
   }
 
   getBase64File(img: HTMLImageElement): string | undefined {
-    if (!this.context) return undefined;
     const imageRatio =
       img.width > img.height ? img.width / 40 : img.height / 40;
+    this.canvas.width = img.width / imageRatio;
+    this.canvas.height = img.height / imageRatio;
+    if (!this.context) return undefined;
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.context.drawImage(
-      img,
-      0,
-      0,
-      img.width / imageRatio,
-      img.height / imageRatio,
-    );
+    this.context.drawImage(img, 0, 0, this.canvas.width, this.canvas.height);
     const dataURL = this.canvas.toDataURL('image/png');
     return dataURL.slice(dataURL.indexOf('base64,') + 7);
   }
