612333d05ad1329411e0f3347a12a4e38eb8a78b
mrkavalsky <mr.kavalsky@gmail.com>
2021-05-30 22:45:40 +0300

    feat: add user avatars to the best score page

--- match-match-game/src/components/user/user.ts
+++ match-match-game/src/components/user/user.ts
@@ -2,15 +2,24 @@ import { BaseBlock } from '../../shared/base-block';
 import { BaseComponent } from '../../shared/base-component';
 import { IUser } from '../../shared/user-interface';
 import './user.css';
+import defaultAvatar from '../../assets/images/default-avatar.png';
 
 export class User extends BaseBlock {
-  private userInfo: BaseBlock = new BaseBlock('div');
+  private userInfo: BaseBlock = new BaseBlock('div', ['user__info']);
 
   private userScore: BaseBlock = new BaseBlock('div', ['user__score']);
 
   constructor(private user: IUser) {
     super('div', ['user']);
+    const userAvatar: BaseComponent = new BaseComponent('img', [
+      'user__avatar',
+    ]);
+    const image: HTMLImageElement = userAvatar.element as HTMLImageElement;
+    image.src = this.user.avatar
+      ? `data:image/png;base64,${this.user.avatar}`
+      : defaultAvatar;
     this.userInfo.appendComponents([
+      userAvatar,
       new BaseComponent(
         'div',
         ['user__name'],
