e8de714115f94f0b801257ff09b87176c9f544b5
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-04 16:33:50 +0300

    feat: add the pageName variable to the BasePage class

--- async-race/src/shared/base-page.ts
+++ async-race/src/shared/base-page.ts
@@ -5,14 +5,17 @@ export class BasePage extends BaseComponent {
 
   private paginationElement: BaseComponent;
 
+  private pageName: string;
+
   constructor(className: string) {
     super(null, 'main', className);
-    const pageName = className[0].toUpperCase() + className.slice(1);
+    [this.pageName] = className.split('-');
+    const caption = this.pageName[0].toUpperCase() + this.pageName.slice(1);
     this.title = new BaseComponent(
       this.node,
       'h2',
       `${className}__title`,
-      `${pageName} (0)`,
+      `${caption} (0)`,
     );
     this.paginationElement = new BaseComponent(
       this.node,
