bdd04419b401e8417f5e5a96282cd92245e3357f
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-04 15:28:50 +0300

    feat: add the header on the page

--- async-race/src/app.ts
+++ async-race/src/app.ts
@@ -1,5 +1,9 @@
 import './app.css';
+import { Header } from './components/header';
 
 export class App {
-  constructor(private rootElement: HTMLElement) {}
+  constructor(private rootElement: HTMLElement) {
+    const header: Header = new Header(rootElement);
+    rootElement.append(header.node);
+  }
 }
