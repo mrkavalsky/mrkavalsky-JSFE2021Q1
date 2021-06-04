dc6744e9d35b33582e736c7b3694957eb3cb8bfd
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-04 17:39:40 +0300

    fix: fix the response in the getTotalCount method

--- async-race/src/components/async-race-api.ts
+++ async-race/src/components/async-race-api.ts
@@ -2,7 +2,7 @@ export class AsyncRaceApi {
   private baseUrl = 'http://localhost:3000';
 
   async getTotalCount(path: string): Promise<string | null> {
-    const response = await fetch(`${this.baseUrl}/${path}`);
+    const response = await fetch(`${this.baseUrl}/${path}?_limit=1`);
     return response.headers.get('X-Total-Count');
   }
 }
