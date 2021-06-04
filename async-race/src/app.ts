82c1d775f1e35c786f242ad0acf12fdabda09688
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-04 15:34:17 +0300

    feat: add the pages to the App class

--- async-race/src/app.ts
+++ async-race/src/app.ts
@@ -1,9 +1,15 @@
 import './app.css';
 import { Header } from './components/header';
+import { BasePage } from './shared/base-page';
 
 export class App {
+  private garagePage = new BasePage('garage-page');
+
+  private winnersPage = new BasePage('winners-page');
+
   constructor(private rootElement: HTMLElement) {
     const header: Header = new Header(rootElement);
     rootElement.append(header.node);
+    rootElement.append(this.garagePage.node);
   }
 }
