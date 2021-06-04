5a04c864718b17ef1728bc21ee08b0f0c00b3802
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-04 16:23:20 +0300

    feat: implement the getTotalCount method in the AsyncRaceApi class

--- async-race/src/components/async-race-api.ts
+++ async-race/src/components/async-race-api.ts
@@ -1,3 +1,8 @@
 export class AsyncRaceApi {
   private baseUrl = 'http://localhost:3000';
+
+  async getTotalCount(path: string): Promise<string | null> {
+    const response = await fetch(`${this.baseUrl}/${path}`);
+    return response.headers.get('X-Total-Count');
+  }
 }
