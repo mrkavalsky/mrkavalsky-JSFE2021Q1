1224ccb8e9b4fa4c12f2bf8cad65e3b89036ac3a
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-07 16:31:11 +0300

    feat: implement the refreshCarListPage method in the Carlist class

--- async-race/src/components/car-list/car-list.ts
+++ async-race/src/components/car-list/car-list.ts
@@ -1,10 +1,15 @@
 import { BaseComponent } from '../../shared/base-component';
+import { ICar } from '../../shared/ICar';
 import { CarListPage } from '../car-list-page/car-list-page';
 
 export class CarList extends BaseComponent {
-  private startPage: CarListPage = new CarListPage(this.node);
+  private carListPage: CarListPage = new CarListPage(this.node);
 
   constructor(parentNode: HTMLElement) {
     super(parentNode, 'div', 'car-list', 'Page #1');
+  }
+
+  refreshCarListPage(cars: ICar[]): void {
+    this.carListPage = new CarListPage(this.node, cars);
   }
 }
