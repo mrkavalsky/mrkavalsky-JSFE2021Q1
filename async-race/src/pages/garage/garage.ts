4e54309a088fa002c953074fc867feb18e60e2a8
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-07 17:49:42 +0300

    feat: add the instance of the GarageCarList class to the Garage page

--- async-race/src/pages/garage/garage.ts
+++ async-race/src/pages/garage/garage.ts
@@ -1,8 +1,12 @@
+import { GarageCarList } from '../../components/garage-car-list/garage-car-list';
 import { BasePage } from '../../shared/base-page';
 
 export class Garage extends BasePage {
+  private carList: GarageCarList;
+
   constructor() {
     super('garage');
+    this.carList = new GarageCarList(this.node);
     this.refreshCarList();
   }
 
