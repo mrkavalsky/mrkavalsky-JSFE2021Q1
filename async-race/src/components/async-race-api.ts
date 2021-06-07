3693a072e045effd5a1dbf5061ef5b69ecd83f12
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-07 16:45:55 +0300

    feat: implement the getGarageCars in the AsyncRaceApi class

--- async-race/src/components/async-race-api.ts
+++ async-race/src/components/async-race-api.ts
@@ -1,8 +1,15 @@
+import { ICar } from '../shared/ICar';
+
 export class AsyncRaceApi {
   private baseUrl = 'http://localhost:3000';
 
   async getTotalCount(path: string): Promise<string | null> {
     const response = await fetch(`${this.baseUrl}/${path}?_limit=1`);
     return response.headers.get('X-Total-Count');
+  }
+
+  async getGarageCars(path: string): Promise<ICar[]> {
+    const response = await fetch(`${this.baseUrl}/${path}`);
+    return response.json();
   }
 }
