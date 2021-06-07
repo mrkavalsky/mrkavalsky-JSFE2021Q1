81e943afd708f309c3556f857f0cb06571ff9165
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-07 17:39:09 +0300

    fix: remove the refreshCarListPage method from the BaseCarList class

--- async-race/src/shared/base-car-list/base-car-list.ts
+++ async-race/src/shared/base-car-list/base-car-list.ts
@@ -1,15 +1,7 @@
 import { BaseComponent } from '../base-component';
-import { ICar } from '../ICar';
-import { CarListPage } from '../../components/car-list-page/car-list-page';
 
 export class BaseCarList extends BaseComponent {
-  private carListPage: CarListPage = new CarListPage(this.node);
-
   constructor(parentNode: HTMLElement) {
     super(parentNode, 'div', 'car-list', 'Page #1');
-  }
-
-  refreshCarListPage(cars: ICar[]): void {
-    this.carListPage = new CarListPage(this.node, cars);
   }
 }
