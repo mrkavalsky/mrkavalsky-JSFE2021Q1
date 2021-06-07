b1a952e7a3c81c643a82af3825010982106b07c3
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-07 17:41:20 +0300

    feat: add the button to the BaseCarList class

--- async-race/src/shared/base-car-list/base-car-list.ts
+++ async-race/src/shared/base-car-list/base-car-list.ts
@@ -1,6 +1,20 @@
 import { BaseComponent } from '../base-component';
 
 export class BaseCarList extends BaseComponent {
+  public readonly prevPage: BaseComponent = new BaseComponent(
+    this.node,
+    'button',
+    'button',
+    'prev',
+  );
+
+  public readonly nextPage: BaseComponent = new BaseComponent(
+    this.node,
+    'button',
+    'button',
+    'next',
+  );
+
   constructor(parentNode: HTMLElement) {
     super(parentNode, 'div', 'car-list', 'Page #1');
   }
