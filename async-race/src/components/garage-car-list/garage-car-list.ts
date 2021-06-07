1cfaa1a7a3f023c2b247b5b867f4a2ee9c3c0330
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-07 17:45:55 +0300

    feat: implement the GarageCarList class

---
+++ async-race/src/components/garage-car-list/garage-car-list.ts
@@ -0,0 +1,11 @@
+import { BaseCarList } from '../../shared/base-car-list/base-car-list';
+import { ICar } from '../../shared/ICar';
+import { GarageCarListPage } from '../garage-car-list-page/garage-car-list-page';
+
+export class GarageCarList extends BaseCarList {
+  private carListPage: GarageCarListPage = new GarageCarListPage(this.node);
+
+  refreshCarListPage(cars: ICar[]): void {
+    this.carListPage = new GarageCarListPage(this.node, cars);
+  }
+}
