7248a0730ddd3e3877a43b777a7a25ddaa984d93
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-07 18:18:50 +0300

    refactor: replace the instances of the BaseComponent class with the instances of the BaseButton class

--- async-race/src/shared/base-car-list/base-car-list.ts
+++ async-race/src/shared/base-car-list/base-car-list.ts
@@ -1,19 +1,10 @@
+import { BaseButton } from '../base-button';
 import { BaseComponent } from '../base-component';
 
 export class BaseCarList extends BaseComponent {
-  public readonly prevPage: BaseComponent = new BaseComponent(
-    this.node,
-    'button',
-    'button',
-    'prev',
-  );
+  public readonly prevPage: BaseButton = new BaseButton(this.node, 'prev');
 
-  public readonly nextPage: BaseComponent = new BaseComponent(
-    this.node,
-    'button',
-    'button',
-    'next',
-  );
+  public readonly nextPage: BaseButton = new BaseButton(this.node, 'next');
 
   constructor(parentNode: HTMLElement) {
     super(parentNode, 'div', 'car-list', 'Page #1');
