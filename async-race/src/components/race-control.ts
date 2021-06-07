27011509919cc75994f83b78078f0a2b8bd923d4
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-07 13:44:01 +0300

    feat: create the base constructor of the RaceControl class

---
+++ async-race/src/components/race-control.ts
@@ -0,0 +1,15 @@
+import { BaseComponent } from '../shared/base-component';
+import { ICar } from '../shared/ICar';
+import { Car } from './car/car';
+import { RaceTrack } from './race-track/race-track';
+
+export class RaceControl extends BaseComponent {
+  private car = new Car(this.node);
+
+  private raceTrack = new RaceTrack(this.node);
+
+  constructor(parentNode: HTMLElement, carInfo: ICar) {
+    super(parentNode, 'div', 'race-control', carInfo.name);
+    this.car.setColor(carInfo.color);
+  }
+}
