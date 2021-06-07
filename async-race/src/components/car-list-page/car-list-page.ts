108ff4212a82b487789cd0b054d6ae463e35f53c
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-07 13:56:17 +0300

    feat: implement the CarListPage class

---
+++ async-race/src/components/car-list-page/car-list-page.ts
@@ -0,0 +1,21 @@
+import { BaseComponent } from '../../shared/base-component';
+import { ICar } from '../../shared/ICar';
+import { RaceControl } from '../race-control';
+
+export class CarListPage extends BaseComponent {
+  private carArray: RaceControl[] = [];
+
+  constructor(parentNode: HTMLElement, cars: ICar[] = []) {
+    super(parentNode, 'div', 'car-list__page');
+    this.initGarage(cars);
+  }
+
+  addCar(carInfo: ICar): void {
+    const car: RaceControl = new RaceControl(this.node, carInfo);
+    this.carArray.push(car);
+  }
+
+  initGarage(cars: ICar[]): void {
+    cars.forEach((car) => this.addCar(car));
+  }
+}
