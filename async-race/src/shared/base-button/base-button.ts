3861a4d923a38832bc3d03140d7bd35686d3db98
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-08 14:44:36 +0300

    feat: add the CSS styles to the button element

--- async-race/src/shared/base-button/base-button.ts
+++ async-race/src/shared/base-button/base-button.ts
@@ -1,4 +1,5 @@
 import { BaseComponent } from '../base-component';
+import './base-button.css';
 
 export class BaseButton extends BaseComponent {
   constructor(parentNode: HTMLElement, content: string) {
