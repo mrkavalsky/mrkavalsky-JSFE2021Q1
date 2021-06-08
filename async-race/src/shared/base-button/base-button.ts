c9a8dbfec26e46c765fac482ba37727cb3d52b20
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-08 17:30:42 +0300

    feat: implement the ability to switch the mode of the button

--- async-race/src/shared/base-button/base-button.ts
+++ async-race/src/shared/base-button/base-button.ts
@@ -5,4 +5,12 @@ export class BaseButton extends BaseComponent {
   constructor(parentNode: HTMLElement, content: string) {
     super(parentNode, 'button', 'button', content);
   }
+
+  setDisable(): void {
+    this.node.classList.add('button_disable');
+  }
+
+  setEnable(): void {
+    this.node.classList.remove('button_disable');
+  }
 }
