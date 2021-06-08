0f3a595a21c4e818798b6a4034ffa9f02d405f10
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-08 11:26:05 +0300

    implement the generateCars method in the Gatage class

--- async-race/src/pages/garage/garage.ts
+++ async-race/src/pages/garage/garage.ts
@@ -1,3 +1,4 @@
+import { getCars } from '../../components/cars-generator';
 import { GarageCarList } from '../../components/garage-car-list/garage-car-list';
 import { GarageControl } from '../../components/garage-control/garage-control';
 import { BasePage } from '../../shared/base-page';
@@ -10,11 +11,21 @@ export class Garage extends BasePage {
   constructor() {
     super('garage');
     this.carList = new GarageCarList(this.node);
+    this.garageControl.generateCarsButton.node.addEventListener('click', () =>
+      this.generateCars(),
+    );
     this.refreshCarList();
   }
 
   async refreshCarList(): Promise<void> {
     const cars = await this.asyncRaceApi.getGarageCars();
     this.carList.refreshCarListPage(cars);
+  }
+
+  async generateCars(): Promise<void> {
+    const cars = await getCars();
+    cars.forEach(async (car) => this.asyncRaceApi.postCar(car));
+    this.refreshTotalCount();
+    this.refreshCarList();
   }
 }
