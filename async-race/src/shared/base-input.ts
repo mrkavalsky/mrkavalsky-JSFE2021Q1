d01da8e1b7e66108ac8fe209fba5974c65829627
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-08 16:29:51 +0300

    feat: implement the BaseInput class

---
+++ async-race/src/shared/base-input.ts
@@ -0,0 +1,10 @@
+import { BaseComponent } from './base-component';
+
+export class BaseInput extends BaseComponent {
+  public inputNode: HTMLInputElement;
+
+  constructor(parentNode: HTMLElement, className: string) {
+    super(parentNode, 'input', className);
+    this.inputNode = this.node as HTMLInputElement;
+  }
+}
