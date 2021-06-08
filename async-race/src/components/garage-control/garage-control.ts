fe85fc4be3ab88bc8f9471a456511c7be5a73e75
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-08 17:11:43 +0300

    feat: add the car controls on the page

--- async-race/src/components/garage-control/garage-control.ts
+++ async-race/src/components/garage-control/garage-control.ts
@@ -1,7 +1,12 @@
 import { BaseButton } from '../../shared/base-button/base-button';
 import { BaseComponent } from '../../shared/base-component';
+import { CarControl } from '../car-control/car-control';
 
 export class GarageControl extends BaseComponent {
+  public readonly carControlCreate = new CarControl(this.node, 'create');
+
+  public readonly carControlUpdate = new CarControl(this.node, 'update');
+
   public readonly raceButton: BaseButton = new BaseButton(this.node, 'race');
 
   public readonly resetButton: BaseButton = new BaseButton(this.node, 'reset');
