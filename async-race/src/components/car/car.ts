7318841c2d724b38cd05820cdbfa286a733f8c92
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-11 21:08:21 +0300

    feat: implement controlling a car animation

--- async-race/src/components/car/car.ts
+++ async-race/src/components/car/car.ts
@@ -9,4 +9,20 @@ export class Car extends BaseComponent {
   setColor(color: string): void {
     this.node.style.backgroundColor = color;
   }
+
+  moveCar(): void {
+    this.node.style.left = 'calc(100% - 120px)';
+  }
+
+  returnBackCar(): void {
+    this.node.style.left = '0';
+  }
+
+  stopCar(): void {
+    this.node.style.left = `${this.node.getBoundingClientRect().left}px`;
+  }
+
+  setTransitionTime(time: string): void {
+    this.node.style.transitionDuration = time;
+  }
 }
