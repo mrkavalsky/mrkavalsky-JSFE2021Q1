03dc619f7b79211fe1f8f979d9486d0bc8ca70e3
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-13 19:26:38 +0300

    feat: implement controlling a buttons mode in the GarageControl class

--- async-race/src/components/garage-control/garage-control.ts
+++ async-race/src/components/garage-control/garage-control.ts
@@ -20,4 +20,28 @@ export class GarageControl extends BaseComponent {
     super();
     parentNode.prepend(this.node);
   }
+
+  toggleCarAdjustButtonsMode(isDisable = true): void {
+    this.carAdjustCreate.submitButton.toggleButtonMode(isDisable);
+    this.carAdjustUpdate.submitButton.toggleButtonMode(isDisable);
+  }
+
+  toggleRaceButtonMode(isDisable = true): void {
+    this.raceButton.toggleButtonMode(isDisable);
+  }
+
+  toggleResetButtonMode(isDisable = true): void {
+    this.resetButton.toggleButtonMode(isDisable);
+  }
+
+  toggleGenerateButtonMode(isDisable = true): void {
+    this.generateCarsButton.toggleButtonMode(isDisable);
+  }
+
+  toggleAllButtonsMode(isDisable = true): void {
+    this.toggleCarAdjustButtonsMode(isDisable);
+    this.toggleGenerateButtonMode(isDisable);
+    this.toggleRaceButtonMode(isDisable);
+    this.toggleResetButtonMode(isDisable);
+  }
 }
