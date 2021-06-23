b6787f5b3ae3c4671be5a7e5242975821a85c120
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-23 14:57:47 +0300

    feat: change the form-check-label font size

--- english-for-kids-base/src/render/header/header.ts
+++ english-for-kids-base/src/render/header/header.ts
@@ -9,7 +9,7 @@ export const renderHeader = (): void => {
         <a class="navbar-brand" href="#">English For Kids</a>
         <div class="form-check form-switch">
           <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
-          <label class="form-check-label" for="flexSwitchCheckDefault">Train</label>
+          <label class="form-check-label form-check-label_1-5rem" for="flexSwitchCheckDefault">Train</label>
         </div>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01"
         aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
