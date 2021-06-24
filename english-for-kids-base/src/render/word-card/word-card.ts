1283dee67d7d1d0a2c642b3222f43261b24bb936
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-24 14:21:34 +0300

    feat: create the word card element

---
+++ english-for-kids-base/src/render/word-card/word-card.ts
@@ -0,0 +1,33 @@
+import { ICardInfo } from '../../interfaces';
+import './word-card.css';
+
+export const renderWordCard = ({
+  word,
+  translation,
+  image,
+  audioSrc,
+}: ICardInfo): void => {
+  const card = document.createElement('div');
+
+  card.className = 'word-card-container';
+
+  card.dataset.audio = audioSrc;
+
+  card.innerHTML = `
+    <div class="word-card">
+      <div class="word-card__side">
+        <img class="word-card__img" src="${image}" alt="${word}">
+        <div class="word-card__header">
+          ${word}
+          <button type="button" class="btn btn-secondary btn_rotate"></button>
+        </div>
+      </div>
+      <div class="word-card__side word-card__side_back">
+        <img class="word-card__img" src="${image}" alt="${translation}">
+        <div class="word-card__header">${translation}</div>
+      </div>
+    </div>
+  `;
+
+  document.body.append(card);
+};
