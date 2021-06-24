af77f46ed43892463f56df94834999ab1d208411
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-24 19:20:26 +0300

    feat: add the CSS styles to the document

--- english-for-kids-base/src/app.ts
+++ english-for-kids-base/src/app.ts
@@ -3,6 +3,7 @@ import { initScripts } from './components/initScripts';
 import { initApp } from './redux/actions';
 import { store } from './redux/store';
 import { renderApp } from './render/renderApp';
+import './styles.css';
 
 export const app = (): void => {
   renderApp();
