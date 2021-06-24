0ba8079b7b39b4795e6f95f80c34f9c2f5553aa6
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-24 21:15:13 +0300

    feat: implement toggling the burger menu on the button clicking

--- english-for-kids-base/src/components/header.ts
+++ english-for-kids-base/src/components/header.ts
@@ -1,6 +1,6 @@
-import { changeTheme } from '../redux/actions';
+import { changeTheme, toggleMenu } from '../redux/actions';
 import { store } from '../redux/store';
-import { THEME_PLAY, THEME_TRAIN } from '../redux/themes';
+import { BURGER_MENU_HIDDEN, THEME_PLAY, THEME_TRAIN } from '../redux/classes';
 
 const changeBodyClass = (): void => {
   const { theme } = store.getState();
@@ -8,8 +8,17 @@ const changeBodyClass = (): void => {
   document.body.className = theme.value;
 };
 
+const toggleBurgerMenu = (): void => {
+  const { menu } = store.getState();
+  const burgerMenu = document.getElementById('burger-menu');
+
+  burgerMenu?.classList.toggle(BURGER_MENU_HIDDEN, menu.isHidden);
+};
+
 export const initHeader = (): void => {
   const checkbox = document.getElementById('flexSwitchCheckDefault');
+  const menuButton = document.getElementById('menu-button');
+  const burgerMenu = document.getElementById('burger-menu');
 
   checkbox?.addEventListener('click', () => {
     const newTheme = document.body.classList.contains(THEME_TRAIN)
@@ -18,6 +27,12 @@ export const initHeader = (): void => {
 
     store.dispatch(changeTheme(newTheme));
   });
+  menuButton?.addEventListener('click', () => {
+    const isHidden = !burgerMenu?.classList.contains(BURGER_MENU_HIDDEN);
+
+    store.dispatch(toggleMenu(isHidden));
+  });
 
   store.subscribe(changeBodyClass);
+  store.subscribe(toggleBurgerMenu);
 };
--- english-for-kids-base/src/redux/classes.ts
+++ english-for-kids-base/src/redux/classes.ts
@@ -1,2 +1,3 @@
 export const THEME_TRAIN = 'theme-train';
 export const THEME_PLAY = 'theme-play';
+export const BURGER_MENU_HIDDEN = 'burger-menu_hidden';
