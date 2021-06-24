fea212c2c814ad7d1cef55e6318bb05632883ac0
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-24 15:31:36 +0300

    feat: add the burger menu and the main page on the page

--- english-for-kids-base/src/render/renderApp.ts
+++ english-for-kids-base/src/render/renderApp.ts
@@ -1,5 +1,9 @@
+import { renderBurgerMenu } from './burger-menu/burger-menu';
 import { renderHeader } from './header/header';
+import { renderMainPage } from './main-page/main-page';
 
 export const renderApp = (): void => {
   renderHeader();
+  renderBurgerMenu();
+  renderMainPage();
 };
