0e1373666b45c52a43c438fe44f9f8cacce58a8e
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-24 15:06:40 +0300

    feat: create the menu card element

---
+++ english-for-kids-base/src/render/menu-card/menu-card.ts
@@ -0,0 +1,18 @@
+import { ICards } from '../../interfaces';
+import './menu-card.css';
+
+export const renderMenuCard = ({ category, cards }: ICards): HTMLDivElement => {
+  const card = document.createElement('div');
+
+  card.className = 'menu-card card border-secondary';
+
+  const [{ image }] = cards;
+  card.innerHTML = `
+    <div class="menu-card__img-wrapper">
+      <img class="menu-card__img" src="${image}" alt="${category}">
+    </div>
+    <div class="menu-card__header">${category}</div>
+  `;
+
+  return card;
+};
