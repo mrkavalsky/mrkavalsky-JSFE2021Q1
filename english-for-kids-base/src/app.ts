1ce82fe2029dc05a09c64062fb65de717abcaea6
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-25 19:57:38 +0300

    refactor: remove call the initScripts function

--- english-for-kids-base/src/app.ts
+++ english-for-kids-base/src/app.ts
@@ -1,5 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { initScripts } from './components/initScripts';
 import { initApp } from './actions/actions';
 import { store } from './redux/store';
 import { renderApp } from './components/renderApp';
@@ -7,6 +6,5 @@ import './styles.css';
 
 export const app = (): void => {
   renderApp();
-  initScripts();
   store.dispatch(initApp());
 };
