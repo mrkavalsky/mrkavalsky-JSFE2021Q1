81db01bc176850b9adee102e154b1511776b1a31
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-27 13:55:19 +0300

    feat: implement calling the initApp function after downloading the page

--- english-for-kids-base/src/index.ts
+++ english-for-kids-base/src/index.ts
@@ -2,8 +2,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { initDefaultStore } from './actions/actions';
 import { renderApp } from './components/renderApp';
 import './styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export const initApp = (): void => {
+const initApp = (): void => {
   renderApp();
   initDefaultStore();
 };
+
+window.addEventListener('load', initApp);
