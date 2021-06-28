4dc8e069896ad466359c21fa5b5003b1c715932a
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-28 17:31:04 +0300

    refactor: remove conditional branching from the getCurrentCards function

--- english-for-kids-base/src/get-current-cards.ts
+++ english-for-kids-base/src/get-current-cards.ts
@@ -4,6 +4,5 @@ import { ICardInfo } from './interfaces';
 export const getCurrentCards = (currentHash: string): ICardInfo[] | void => {
   const currentCards = cards.find(({ hash }) => hash === currentHash);
 
-  if (!currentCards) return currentCards;
-  return currentCards.cardsList;
+  return currentCards?.cardsList;
 };
