a8fd6232fbc16f6e63c2671d68f0745ca6da28e0
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-28 18:31:44 +0300

    refactor: remove the page state from the initial state

--- english-for-kids-base/src/reducers/initial-state.ts
+++ english-for-kids-base/src/reducers/initial-state.ts
@@ -1,12 +1,8 @@
 import { Theme } from '../components/header/classes';
-import { MAIN_PAGE } from '../components/main-page/config';
 
 const initialState = {
   theme: {
     value: Theme.THEME_TRAIN,
-  },
-  page: {
-    value: MAIN_PAGE,
   },
 };
 
