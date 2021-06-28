b358a7f3a5aa3542f1c8e12ad54dd06ec0c64f00
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-28 20:08:17 +0300

    refactor: replace creating an element in the word card with calling the createHTMLElement function

--- english-for-kids-base/src/components/word-card/word-card.ts
+++ english-for-kids-base/src/components/word-card/word-card.ts
@@ -1,3 +1,4 @@
+import { createHTMLElement } from '../../helpers/create-html-element';
 import { ICardInfo } from '../../types/interfaces';
 import './word-card.css';
 
@@ -5,28 +6,24 @@ export const renderWordCard = ({
   word,
   translation,
   image,
-}: ICardInfo): HTMLDivElement => {
-  const card = document.createElement('div');
-
-  card.className = 'word-card-container';
-
-  card.dataset.word = word;
-
-  card.innerHTML = `
-    <div class="word-card">
-      <div class="word-card__side">
-        <img class="word-card__img" src="${image}" alt="${word}">
-        <div class="word-card__header">
-          ${word}
-          <button type="button" class="btn btn-secondary btn_rotate"></button>
+}: ICardInfo): Element => {
+  const card = createHTMLElement(`
+    <div class="word-card-container">
+      <div class="word-card">
+        <div class="word-card__side">
+          <img class="word-card__img" src="${image}" alt="${word}">
+          <div class="word-card__header">
+            ${word}
+            <button type="button" class="btn btn-secondary btn_rotate"></button>
+          </div>
+        </div>
+        <div class="word-card__side word-card__side_back">
+          <img class="word-card__img" src="${image}" alt="${translation}">
+          <div class="word-card__header">${translation}</div>
         </div>
       </div>
-      <div class="word-card__side word-card__side_back">
-        <img class="word-card__img" src="${image}" alt="${translation}">
-        <div class="word-card__header">${translation}</div>
-      </div>
     </div>
-  `;
+  `);
 
   return card;
 };
