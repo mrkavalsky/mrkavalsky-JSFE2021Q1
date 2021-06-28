0d6d0512b02ecc130480fbbee95d6f41163093ad
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-28 20:07:38 +0300

    refactor: replace creating an element in the main page with calling the createHTMLElement function

--- english-for-kids-base/src/components/main-page/main-page.ts
+++ english-for-kids-base/src/components/main-page/main-page.ts
@@ -1,11 +1,12 @@
 import { cards } from '../../../public/cards';
+import { createHTMLElement } from '../../helpers/create-html-element';
 import { renderMenuCard } from '../menu-card/menu-card';
 import './main-page.css';
 
 export const renderMainPage = (): void => {
-  const main = document.createElement('main');
-
-  main.className = 'main-page';
+  const main = createHTMLElement(`
+    <main class="main-page"></main>
+  `);
 
   cards.forEach((card) => {
     main.append(renderMenuCard(card));
