bb3b32725bf9bbef8699dfa1cc992ee3fd422057
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-29 10:53:40 +0300

    refactor: implement the getHash function

--- english-for-kids-base/src/router/add-routing.ts
+++ english-for-kids-base/src/router/add-routing.ts
@@ -1,7 +1,8 @@
 import { MAIN_PAGE } from '../components/main-page/config';
 import { changeCurrentPage } from './change-current-page';
+import { getHash } from './get-hash';
 
 export const addRouting = (): void => {
   window.location.hash = MAIN_PAGE;
-  window.onpopstate = () => changeCurrentPage(window.location.hash.slice(1));
+  window.onpopstate = () => changeCurrentPage(getHash());
 };
---
+++ english-for-kids-base/src/router/get-hash.ts
@@ -0,0 +1,1 @@
+export const getHash = (): string => window.location.hash.slice(1);
