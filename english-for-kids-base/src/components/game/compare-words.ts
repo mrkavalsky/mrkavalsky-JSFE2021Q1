628e53df2d24536fcac7174b7bfe09df3b048883
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-29 14:15:57 +0300

    feat: implement the compareWords function

---
+++ english-for-kids-base/src/components/game/compare-words.ts
@@ -0,0 +1,9 @@
+import { hitWord, missWord } from '../../actions/actions';
+
+export const compareWords = (currentWord: string, playerWord: string): void => {
+  if (currentWord === playerWord) {
+    hitWord();
+  } else {
+    missWord();
+  }
+};
