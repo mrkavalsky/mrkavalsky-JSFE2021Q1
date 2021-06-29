496457507546da7fe869176e0c51d20274ce3acb
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-29 21:44:55 +0300

    refactor: rename the main-card.ts file to index

--- english-for-kids-base/src/components/main-page/index.ts
+++ english-for-kids-base/src/components/main-page/index.ts
@@ -1,6 +1,6 @@
 import { cards } from '../../../public/cards';
 import { createHTMLElement } from '../../helpers/create-html-element';
-import { renderMenuCard } from '../menu-card/menu-card';
+import { renderMenuCard } from '../menu-card';
 import './main-page.css';
 
 export const renderMainPage = (): void => {
