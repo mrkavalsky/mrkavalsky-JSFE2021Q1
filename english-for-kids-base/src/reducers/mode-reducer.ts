d9e95c9da4c88dc0810d65954ff3663cb459c0ef
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-30 18:54:44 +0300

    feat: update the mode reducer by the interfaces

--- english-for-kids-base/src/reducers/mode-reducer.ts
+++ english-for-kids-base/src/reducers/mode-reducer.ts
@@ -1,10 +1,11 @@
 import { CHANGE_MODE } from '../actions/action-types';
+import { IModeAction, IModeState } from '../types/interfaces';
 import initialState from './initial-state';
 
 export const modeReducer = (
-  state: any = initialState.mode,
-  { type, payload }: any,
-): any => {
+  state: IModeState = initialState.mode,
+  { type, payload }: IModeAction,
+): IModeState => {
   switch (type) {
     case CHANGE_MODE:
       return { ...state, value: payload };
