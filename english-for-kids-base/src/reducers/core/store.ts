ed566fd86ba9246afcc9302abeb702dc3f2c079d
mrkavalsky <mr.kavalsky@gmail.com>
2021-06-30 17:53:39 +0300

    refactor: rename the rootReducer file to root-reducer

--- english-for-kids-base/src/reducers/core/store.ts
+++ english-for-kids-base/src/reducers/core/store.ts
@@ -1,7 +1,7 @@
 import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import initialState from '../initial-state';
-import { rootReducer } from '../rootReducer';
+import { rootReducer } from '../root-reducer';
 
 const initStore = () => {
   return createStore(rootReducer, initialState, composeWithDevTools());
