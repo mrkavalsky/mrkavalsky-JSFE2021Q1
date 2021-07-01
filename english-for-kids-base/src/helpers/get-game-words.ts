ae6adc3868da9dd10bdc30d093b610dd14cb621d
mrkavalsky <mr.kavalsky@gmail.com>
2021-07-01 21:16:49 +0300

    fix: fix the getGameWord function according to the gameWord interface

--- english-for-kids-base/src/helpers/get-game-words.ts
+++ english-for-kids-base/src/helpers/get-game-words.ts
@@ -1,12 +1,22 @@
 import { getHash } from '../router/get-hash';
-import { ICardInfo } from '../types/interfaces';
+import { IGameWord } from '../types/interfaces';
 import { getCurrentCards } from './get-current-cards';
 
-export const getGameWords = (): ICardInfo[] | void => {
+export const getGameWords = (): IGameWord[] | void => {
   const cards = getCurrentCards(getHash());
 
   if (cards) {
-    return cards.sort(() => Math.random() - 0.5);
+    return cards
+      .map(({ word, audioSrc, train, miss }) => {
+        return {
+          word,
+          audioSrc,
+          train,
+          isHit: false,
+          miss,
+        };
+      })
+      .sort(() => Math.random() - 0.5);
   }
 
   return cards;
