37808e861c237a0633f27d71565be4cf2b119482
mrkavalsky <mr.kavalsky@gmail.com>
2021-07-01 13:30:20 +0300

    feat: implement transition on a category page in the burger menu

--- english-for-kids-base/src/components/header/burger-menu/index.ts
+++ english-for-kids-base/src/components/header/burger-menu/index.ts
@@ -2,16 +2,23 @@ import { cards } from '../../../../public/cards';
 import { createHTMLElement } from '../../../helpers/create-html-element';
 import './styles.css';
 import { BURGER_MENU_HIDDEN } from './classes';
+import { changeHash } from '../../../router/change-hash';
+
+const addHandler = (listItem: Element, hash: string) => {
+  listItem.addEventListener('click', () => changeHash(hash));
+};
 
 const createBurgerMenuList = (): Element => {
   const burgerMenuList = createHTMLElement(`
     <ul class="list-unstyled"></ul>
   `);
 
-  cards.forEach(({ category }) => {
+  cards.forEach(({ category, hash }) => {
     const listItem = createHTMLElement(`
       <li class="h3 py-1" role="button">${category}</li>
     `);
+
+    addHandler(listItem, hash);
 
     burgerMenuList.append(listItem);
   });
