9469de4133dbadf137e0226bd8ea7e95af860fa9
mrkavalsky <mr.kavalsky@gmail.com>
2021-07-02 22:28:42 +0300

    refactor: repalce the find method to the map method

--- english-for-kids-base/src/helpers/get-updated-cards.ts
+++ english-for-kids-base/src/helpers/get-updated-cards.ts
@@ -2,16 +2,8 @@ import { IGameWord } from '../types/interfaces';
 
 export const getUpdatedCards = (
   currentCards: IGameWord[],
-  { word, isHit, train, miss }: IGameWord,
-): IGameWord[] => {
-  const cards = [...currentCards];
-  const card = cards.find((e) => e.word === word);
-
-  if (card) {
-    card.isHit = isHit;
-    card.miss = miss;
-    card.train = train;
-  }
-
-  return cards;
-};
+  currentCard: IGameWord,
+): IGameWord[] =>
+  currentCards.map((card) =>
+    card.word === currentCard.word ? currentCard : card,
+  );
