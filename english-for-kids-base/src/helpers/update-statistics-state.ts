fa6a8683532fa3100084f15780f416460def53f1
mrkavalsky <mr.kavalsky@gmail.com>
2021-07-03 17:59:12 +0300

    feat: implement displaying the popup

--- english-for-kids-base/src/helpers/update-statistics-state.ts
+++ english-for-kids-base/src/helpers/update-statistics-state.ts
@@ -1,6 +1,7 @@
 import { changeCurrentCard, updateCurrentCards } from '../actions/actions';
 import { IGameWord } from '../types/interfaces';
 import { findCard } from './find-card';
+import { finishGame } from './finish-game';
 import { getUpdatedCards } from './get-updated-cards';
 import { playAudio } from './play-audio';
 
@@ -11,9 +12,12 @@ export const updateStatisticsState = (
   const newCurrentCards = getUpdatedCards(currentCards, currentCard);
   const newCurrentCard = findCard(newCurrentCards);
 
+  updateCurrentCards(newCurrentCards);
+
   if (newCurrentCard) {
     changeCurrentCard(newCurrentCard);
     playAudio(newCurrentCard.audioSrc);
+  } else {
+    finishGame();
   }
-  updateCurrentCards(newCurrentCards);
 };
