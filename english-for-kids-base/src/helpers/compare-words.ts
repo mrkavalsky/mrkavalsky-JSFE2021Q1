fc6b7e2b90147360e6b07fb527039314beb8bf00
mrkavalsky <mr.kavalsky@gmail.com>
2021-07-04 14:16:06 +0300

    feat: implement playing the compare result audio

--- english-for-kids-base/src/helpers/compare-words.ts
+++ english-for-kids-base/src/helpers/compare-words.ts
@@ -1,9 +1,30 @@
 import { hitWord, missWord } from '../actions/actions';
+import { CORRECT_AUDIO, ERROR_AUDIO } from '../components/word-card/config';
 import { store } from '../reducers/core/store';
 import { updateStatisticsState } from './update-statistics-state';
 
-export const compareWords = (currentWord: string, playerWord: string): void => {
-  if (currentWord === playerWord) {
+function playCompareResult(compareResult: boolean): Promise<void> {
+  return new Promise((res) => {
+    const audio = new Audio();
+    const audioSrc = compareResult ? CORRECT_AUDIO : ERROR_AUDIO;
+
+    audio.addEventListener('ended', () => res());
+
+    audio.src = audioSrc;
+    audio.currentTime = 0;
+    audio.play();
+  });
+}
+
+export async function compareWords(
+  currentWord: string,
+  playerWord: string,
+): Promise<void> {
+  const compareResult = currentWord === playerWord;
+
+  await playCompareResult(compareResult);
+
+  if (compareResult) {
     hitWord();
 
     const {
@@ -14,4 +35,4 @@ export const compareWords = (currentWord: string, playerWord: string): void => {
   } else {
     missWord();
   }
-};
+}
