cc1df0e6cb5abaa8e120c886ebac2e4ec6ed59f5
mrkavalsky <mr.kavalsky@gmail.com>
2021-07-07 12:42:09 +0300

    refactor: rewrite conditional branching

--- english-for-kids-base/src/components/statistics-page/helpers/sort_desc.ts
+++ english-for-kids-base/src/components/statistics-page/helpers/sort_desc.ts
@@ -3,14 +3,9 @@ import { IStatisticsCard } from '../../../types/interfaces';
 export const getSortDESC = (key: keyof IStatisticsCard) => {
   return (firstCard: IStatisticsCard, secondCard: IStatisticsCard): number => {
     if (
-      typeof firstCard[key] === 'number' &&
-      firstCard[key] < secondCard[key]
-    ) {
-      return 1;
-    }
-    if (
-      typeof firstCard[key] === 'string' &&
-      firstCard[key] > secondCard[key]
+      (typeof firstCard[key] === 'number' &&
+        firstCard[key] < secondCard[key]) ||
+      (typeof firstCard[key] === 'string' && firstCard[key] > secondCard[key])
     ) {
       return 1;
     }
