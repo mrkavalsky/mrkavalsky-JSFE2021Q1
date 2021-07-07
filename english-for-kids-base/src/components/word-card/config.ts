acc63a1920b5f7f011213b8116b4469e3172312a
mrkavalsky <mr.kavalsky@gmail.com>
2021-07-07 18:09:49 +0300

    fix: fix playing audio

--- english-for-kids-base/src/components/word-card/config.ts
+++ english-for-kids-base/src/components/word-card/config.ts
@@ -1,2 +1,2 @@
-export const ERROR_AUDIO = '/audio/error.mp3';
-export const CORRECT_AUDIO = '/audio/correct.mp3';
+export const ERROR_AUDIO = 'audio/error.mp3';
+export const CORRECT_AUDIO = 'audio/correct.mp3';
