72510054d958599a77dd38847747e4a7804cb48f
mrkavalsky <mr.kavalsky@gmail.com>
2021-07-14 23:12:34 +0300

    feat: implement displaying the static page

--- english-for-kids-admin-panel/src/index.ts
+++ english-for-kids-admin-panel/src/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import path from 'path';
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.use(express.static(path.resolve(__dirname, '../static')));
 
 app.listen(PORT, () => {
   console.log(`Server has been started on port ${PORT}...`);
