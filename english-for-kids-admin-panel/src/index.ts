391cddddace607d021eebcff05e83a8caa6546f7
mrkavalsky <mr.kavalsky@gmail.com>
2021-07-15 15:28:19 +0300

    feat: add cors

--- english-for-kids-admin-panel/src/index.ts
+++ english-for-kids-admin-panel/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import { PORT } from './config';
 import { ROUTER } from './routes';
 
@@ -8,12 +9,15 @@ function startServer(): void {
 
   app.use('/', express.static(path.resolve(__dirname, '../static')));
   app.use('/api/content', express.static(path.resolve(__dirname, '../cards')));
+  app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use('/api/cards', ROUTER);
 
   app.listen(PORT, () => {
-    console.log(`Server has been started on port ${PORT}...`);
+    console.log(
+      `Server has been started on port ${PORT}...`,
+    );
   });
 }
 
