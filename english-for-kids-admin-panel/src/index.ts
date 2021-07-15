b19cb8b646d92410bacaf6f8fc27737c723038e9
mrkavalsky <mr.kavalsky@gmail.com>
2021-07-15 08:01:00 +0300

    refactor: move the port variable to the config

--- english-for-kids-admin-panel/src/index.ts
+++ english-for-kids-admin-panel/src/index.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import path from 'path';
+import { PORT } from './config';
 import { ROUTER } from './routes';
 
-const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../static')));
