247ffcc94f427e9384e11bc0f16a9f95479617a0
mrkavalsky <mr.kavalsky@gmail.com>
2021-07-15 08:23:24 +0300

    feat: implement the startServer function

--- english-for-kids-admin-panel/src/index.ts
+++ english-for-kids-admin-panel/src/index.ts
@@ -3,13 +3,17 @@ import path from 'path';
 import { PORT } from './config';
 import { ROUTER } from './routes';
 
-const app = express();
+function startServer(): void {
+  const app = express();
 
-app.use(express.static(path.resolve(__dirname, '../static')));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(ROUTER);
+  app.use(express.static(path.resolve(__dirname, '../static')));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(ROUTER);
 
-app.listen(PORT, () => {
-  console.log(`Server has been started on port ${PORT}...`);
-});
+  app.listen(PORT, () => {
+    console.log(`Server has been started on port ${PORT}...`);
+  });
+}
+
+startServer();
